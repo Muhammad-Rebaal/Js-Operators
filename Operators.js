46266bf2d71ad86915c794570b4f04487b822464
Muhammad-Rebaal <122050424+Muhammad-Rebaal@users.noreply.github.com>
2023-04-15 01:29:22 +0500

    Add files via upload

---
+++ Operators.js
@@ -0,0 +1,29 @@
+                                        //Operators(Complete)
+
+// Airthmetic Operators
+// console.log(3-2)
+// console.log(3+2)
+// console.log(3*2)
+// console.log(12/2)
+
+// Comparison Operators
+// console.log(12>2)
+// console.log(12<2)
+// console.log(12<=2)
+// console.log(12>=2)
+//  console.log(12==="12")
+
+// Assignment Operator
+// let a = 12
+// a++
+// console.log(a)
+// ++a
+// console.log(a)
+
+// Logical Operators
+// let age = 12
+// if (age==12 || age ==13) {
+//     console.log("Your age is either 12 or 13")
+    
+// }
+// console.log(!true)
